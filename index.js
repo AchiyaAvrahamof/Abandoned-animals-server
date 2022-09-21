6747224acd722574ba96b0ec3e0b95c4028cf845
Achiya Avrahamof <achiyaav001@gmail.com>
2022-09-21 14:41:27 +0300

    models

---
+++ index.js
@@ -0,0 +1,29 @@
+const express=require('express')
+const  mongoose  = require('mongoose')
+const app=express()
+const port=5000
+const routes =require('../server/routes/api')
+const bodyParser=require('body-parser')
+require('dotenv').config()
+
+mongoose.Promise=global.Promise
+mongoose.connect(process.env.DB,{useNewUrlParser:true})
+     .then(()=>{console.log('conected to Data base') })
+     .catch((err)=>console.log(err))
+     app.use(bodyParser.json())
+     app.use('/api',routes)
+     
+     app.use((err,req,res,next)=>{
+    console.log(err);
+    next();
+})
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+  app.use(bodyParser.json());
+app.listen(port,()=>{
+    console.log('server conected on port: '+port);
+})
---
+++ routes/api.js
@@ -0,0 +1,29 @@
+const express=require('express')
+const router=express.Router()
+const Animal=require('../models/animal')
+router.get('/animals',(req,res,next)=>{
+    Animal.find({},["place",'size','color','vailent','problem','time','exstraDetails','photo','type','phoneNumber','email','name'])
+    .then((data) => res.json(data))
+    .catch(next)
+})
+    
+
+router.post('/animals',(req,res,next)=>{
+    req.body.name && req.body.size && req.body.color && req.body.vailent && req.body.problem && req.body.time && req.body.photo && req.body.type && req.body.exstraDetails && req.body.phoneNumber && req.body.email && req.body.name?
+    Animal.create(req.body)
+    .then((data)=>res.json(data))
+    .catch(next):
+    res.json({error:'dont have all the elements'})
+})
+
+router.delete('/animals/:id', ( req,res,next) => {
+    console.log("delete");
+    Animal.findOneAndDelete({_id: req.params.id })
+        .then((data) => res.json(data))
+        .catch(next)
+})
+
+
+
+
+module.exports= router
