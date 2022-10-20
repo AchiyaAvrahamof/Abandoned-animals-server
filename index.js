2899a7c04382f8f79ae5ff26cbca7cf9736c728d
Shahar Zidon <shaharsmacbookpro@Shahars-MacBook-Pro.local>
2022-10-20 14:16:21 +0300

    cyclic

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const express=require('express')
-const  mongoose  = require('mongoose')
-const app=express()
-const port=6000
-const routes =require('../Abandoned-animals-server/routes/api')
-const bodyParser=require('body-parser')
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const port = 6000
+const routes = require('./routes/api')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
-mongoose.Promise=global.Promise
-mongoose.connect(process.env.DB,{useNewUrlParser:true})
-     .then(()=>{console.log('conected to Data base') })
-     .catch((err)=>console.log(err))
-     app.use(bodyParser.json())
-     app.use('/api',routes)
-     
-     app.use((err,req,res,next)=>{
-    console.log(err);
-    next();
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.DB, { useNewUrlParser: true })
+  .then(() => { console.log('conected to Data base') })
+  .catch((err) => console.log(err))
+app.use(bodyParser.json())
+app.use('/api', routes)
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
 })
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
-  app.use(bodyParser.json());
-app.listen(port,()=>{
-    console.log('server conected on port: '+port);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+app.use(bodyParser.json());
+app.listen(port, () => {
+  console.log('server conected on port: ' + port);
 })
