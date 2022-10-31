3922badfabfa90088659ae2354e50908088826c7
Achiya Avrahamof <achiyaav001@gmail.com>
2022-10-31 12:44:36 +0200

    change port to 7000

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const port = 6000
+const port = 7000
 const routes = require('./routes/api')
 const bodyParser = require('body-parser')
 require('dotenv').config()
-
+const cors= require("cors")
+app.use(cors())
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.DB, { useNewUrlParser: true })
   .then(() => { console.log('conected to Data base') })
@@ -18,11 +19,7 @@ app.use((err, req, res, next) => {
   next();
 })
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+
 app.use(bodyParser.json());
 app.listen(port, () => {
   console.log('server conected on port: ' + port);
